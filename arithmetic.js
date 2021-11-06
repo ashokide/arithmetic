071e481470bf524b02d2b11bf1aeab6ed3e3ae85
Ashok N <66304695+ashokide@users.noreply.github.com>
2021-11-06 20:12:48 +0530

    Add files via upload

---
+++ arithmetic.js
@@ -0,0 +1,16 @@
+const add = (a, b) => {
+    return Number(a) + Number(b);
+}
+const sub = (a, b) => {
+    return Number(a) - Number(b);
+}
+const mul = (a, b) => {
+    return Number(a) * Number(b);
+}
+const div = (a, b) => {
+    return Number(a) / Number(b);
+}
+
+module.exports = {
+    add, sub, mul, div
+}
---
+++ index.js
@@ -0,0 +1,31 @@
+const { add, sub, mul, div } = require('./arithmetic.js');
+const express = require('express');
+const app = express()
+
+app.use(express.json())
+
+app.get('/add', (req, res) => {
+    const { a, b } = req.body;
+    const result = add(a, b);
+    res.json({ result });
+})
+app.get('/sub', (req, res) => {
+    const { a, b } = req.body;
+    const result = sub(a, b);
+    res.json({ result });
+})
+app.get('/mul', (req, res) => {
+    const { a, b } = req.body;
+    const result = mul(a, b);
+    res.json({ result });
+})
+app.get('/div', (req, res) => {
+    const { a, b } = req.body;
+    const result = div(a, b);
+    res.json({ result });
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log("Server Started On PORT : " + PORT);
+})
